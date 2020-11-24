180e5a112692372ce9868989060f61d60f3a4936
DSGAMING98 <74608619+DSGAMING98@users.noreply.github.com>
2020-11-24 19:21:41 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -8,7 +8,7 @@ weight=random(400,1500)
 
  car=createSprite(50, 200, 50, 50);
 car.velocityX = speed;
-
+car.shapeColor="CYAN";
 wall=createSprite(1500,200,60,height/2);
 wall.shapeColor=(80,80,80);
 
